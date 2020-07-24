f1fe969116f145b0296ff462cda11513037df68e
vegasLas <68535731+vegasLas@users.noreply.github.com>
2020-07-24 17:00:52 +0300

    added Header.jsx file

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import { NavLink } from 'react-router-dom'
 const Header = (props) => {
     
     return <header className = {classes.header}>
-        <img className = {classes.logo} src={logo} />
+        <img className = {classes.logo} />
         <div className={classes.loginBlock}>
         {props.isAuth
             ? <div>{props.login} -<button onClick={props.logout}>Log out</button></div>
